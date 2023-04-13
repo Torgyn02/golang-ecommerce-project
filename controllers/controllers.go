b450232cf07fa5891ba84c35977d9c1ab8b82a00
Torgyn <bini2002@mail.ru>
2023-04-13 13:19:30 +0600

    commit

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,6 +1,11 @@
 package controllers
 
 import (
+	"context"
+	"golang-ecommerce-project/models"
+	"net/http"
+	"time"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -14,6 +19,21 @@ func VerifyPassword(userPassword string, givenPassword string) (bool, string) {
 
 func SignUp() gin.HandlerFunc {
 
+	return func(c *gin.Context) {
+		var ctx, cansel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cansel()
+
+		var user models.User
+		if err := c.BindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		validationErr := Validate.Struct(user)
+		if validationErr != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": validationErr})
+		}
+	}
 }
 
 func Login() gin.HandlerFunc {
